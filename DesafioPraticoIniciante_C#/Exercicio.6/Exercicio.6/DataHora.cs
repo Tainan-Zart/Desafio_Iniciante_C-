eb862516fefb0a94d43a85df32ea6bf2a600c083
Tainan <tainanzart713@gmail.com>
2024-04-28 17:19:49 -0300

    segundo commit

---
+++ DesafioPraticoIniciante_C#/Exercicio.6/Exercicio.6/DataHora.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace Exercicio._6;
+ class DataHora
+{
+    DateTime dataAtual = DateTime.Now;
+
+    public string DataCompleta(string diaHora)
+    {
+       
+        string formatoCompleto = dataAtual.ToString("dddd, dd 'de' MMMM 'de' yyyy, HH:mm:ss");
+        
+          return formatoCompleto;
+    }  
+
+    public string FormatoData(string diaHora)
+    {
+        
+        string formatoData = dataAtual.ToString("dd/MM/yyyy");
+
+        return formatoData;
+
+    }
+
+    public string MesExtenso(string diaHora)
+    {
+        
+        string formatoMesExtenso = dataAtual.ToString("dd 'de' MMMM 'de' yyyy");
+
+        return formatoMesExtenso;
+
+    }
+
+    public string FormatoHora(string diaHora)
+    {
+        
+        string formatoHora = dataAtual.ToString("HH:mm:ss");
+
+        return formatoHora;
+
+    }
+
+
+
+}
